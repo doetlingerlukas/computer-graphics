332938a519b610d8d82791de268d52cd0bae356f
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2018-01-01 19:47:27 +0100

    Outsource `Ray` and `Image` at Radiosity.

--- Radiosity/Structs.cpp
+++ Radiosity/Structs.cpp
@@ -1,5 +1,10 @@
 #include "Structs.hpp"
 
+/*------------------------------------------------------------------
+| Struct for standard vector operations in 3D 
+| (used for points, vectors, and colors)
+------------------------------------------------------------------*/
+
 Vector::Vector(const Vector &b) : x(b.x), y(b.y), z(b.z) {}
 Vector::Vector(double x_, double y_, double z_) : x(x_), y(y_), z(z_) {}
 
@@ -56,6 +61,61 @@ bool Vector::Equals(const Vector &b) const {
 		return false;
 	}
 }
+
+/*------------------------------------------------------------------
+| Structure for rays (e.g. viewing ray, ray tracing)
+------------------------------------------------------------------*/
+
+Ray::Ray(const Vector org_, const Vector &dir_) : org(org_), dir(dir_) {}
+
+/*------------------------------------------------------------------
+| Struct holds pixels/colors of rendered image
+------------------------------------------------------------------*/
+
+Image::Image(int _w, int _h) : width(_w), height(_h) {
+	pixels = new Color[width * height];        
+}
+
+Color Image::getColor(int x, int y) {
+	int image_index = (height-y-1) * width + x;	
+	return pixels[image_index];
+}
+
+void Image::setColor(int x, int y, const Color &c) {
+	int image_index = (height-y-1) * width + x;	
+	pixels[image_index] = c;
+}
+
+void Image::addColor(int x, int y, const Color &c) {
+	int image_index = (height-y-1) * width + x;	
+	pixels[image_index] = pixels[image_index] + c;
+}
+
+int Image::toInteger(double x) { 
+	/* Clamp to [0,1] */
+	if (x<0.0) x = 0.0;		
+        
+	if (x>1.0) x = 1.0;             
+
+	/* Apply gamma correction and convert to integer */
+	return int(pow(x,1/2.2)*255+.5); 
+}
+
+void Image::Save(const string &filename) {
+	/* Save image in PPM format */
+	FILE *f = fopen(filename.c_str(), "wb");
+	fprintf(f, "P3\n%d %d\n%d\n", width, height, 255);
+	for (int i = 0; i < width * height; i++)
+	fprintf(f,"%d %d %d ", toInteger(pixels[i].x), 
+							toInteger(pixels[i].y), 
+							toInteger(pixels[i].z));
+	fclose(f);
+}
+
+
+
+
+
 
 
 
--- Radiosity/Structs.hpp
+++ Radiosity/Structs.hpp
@@ -13,7 +13,7 @@ using namespace std;
 
 struct Vector {
 	
-    double x, y, z;           /* Position XYZ or color RGB */
+    double x, y, z;			/* Position XYZ or color RGB */
 
     Vector(const Vector &b);
     Vector(double x_=0, double y_=0, double z_=0);
@@ -33,6 +33,25 @@ struct Vector {
 	Vector Cross(const Vector &b) const;
 	Vector Invert() const;
 	bool Equals(const Vector &b) const;
+};
+
+typedef Vector Color;
+
+struct Ray {
+	Vector org, dir;		/* Origin and direction */
+	Ray(const Vector org_, const Vector &dir_);
+};
+
+struct Image {
+    int width, height;
+    Color *pixels;
+
+    Image(int _w, int _h);
+	Color getColor(int x, int y);
+    void setColor(int x, int y, const Color &c);
+    void addColor(int x, int y, const Color &c);
+    int toInteger(double x);
+    void Save(const string &filename);
 };
 
 
